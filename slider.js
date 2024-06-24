16e048d78bb172131c0bd3f4b5ab3e1bee074348
Alejo Carena <alejocarena123@gmail.com>
2024-06-24 14:24:04 -0300

    segunda pagina de proyectos

---
+++ slider.js
@@ -0,0 +1,8 @@
+function sliderR(){
+    document.getElementById('segunda-pagina').classList.remove('ocultar');
+    document.getElementById('segunda-pagina').classList.add('mostrar');
+}
+function sliderL(){
+    document.getElementById('segunda-pagina').classList.remove('mostrar');
+    document.getElementById('segunda-pagina').classList.add('ocultar');
+}
