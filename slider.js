af0b2467ce441891432e09c7fbbd7619c421c882
Alejo Carena <alejocarena123@gmail.com>
2024-06-29 00:00:20 -0300

    correccion de ordenamiento de proyectos

--- slider.js
+++
@@ -1,8 +0,0 @@
-function sliderR(){
-    document.getElementById('segunda-pagina').classList.remove('ocultar');
-    document.getElementById('segunda-pagina').classList.add('mostrar');
-}
-function sliderL(){
-    document.getElementById('segunda-pagina').classList.remove('mostrar');
-    document.getElementById('segunda-pagina').classList.add('ocultar');
-}
